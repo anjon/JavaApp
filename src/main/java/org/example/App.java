2989c4989386a3e521d23f3a9084acf1d47c0b74
Anjon Sarker <anjon.sarker@gmail.com>
2025-02-13 00:47:35 +0100

    1st Test

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,8 +3,5 @@ package org.example;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello from Java App");
-        System.out.println("This is the 2d test");
-        System.out.println("This is the 3rd test");
-        System.out.println("This is the 4th test");
     }
 }
