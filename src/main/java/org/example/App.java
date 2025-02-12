cf659464dd275d956ddf6669d617ceacc528019b
Anjon Sarker <anjon.sarker@gmail.com>
2025-02-13 00:24:35 +0100

    2nd test

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,5 +3,6 @@ package org.example;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello from Java App");
+        System.out.println("This is the 2d test");
     }
 }
